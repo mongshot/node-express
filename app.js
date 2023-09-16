70e62e777e6c4544e652c7a3f4f86f5d7ea4d3b7
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-16 11:07:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const nunjucks = require('nunjucks');
+const TelegramBot = require('node-telegram-bot-api');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+// Telegram 봇 토큰을 여기에 추가합니다.
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
-var app = express();
+// 봇을 생성합니다.
+const bot = new TelegramBot(token, { polling: true });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', { 
-  express: app,
-  watch: true,
+// '/start' 명령어에 대한 핸들러를 추가합니다.
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, '불편사항을 제출하려면 /submit 명령어를 사용하세요.');
 });
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// '/submit' 명령어에 대한 핸들러를 추가합니다.
+bot.onText(/\/submit/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, '불편사항을 입력해주세요.');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// 사용자로부터 메시지를 받는 핸들러를 추가합니다.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (messageText === '/submit') {
+    bot.sendMessage(chatId, '불편사항을 입력해주세요.');
+  } else {
+    // 여기서 메시지를 처리하고 필요한 작업을 수행합니다.
+    // 예를 들어, 불편사항을 저장하거나 처리할 수 있습니다.
+    // 이 예제에서는 단순히 사용자에게 다시 응답합니다.
+    bot.sendMessage(chatId, `당신의 불편사항: ${messageText}`);
+  }
 });
 
-module.exports = app;
+// 봇이 실행 중인지 확인합니다.
+console.log('봇이 실행 중입니다.');
